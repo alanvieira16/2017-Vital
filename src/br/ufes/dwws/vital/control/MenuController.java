39dd3a9050b046b5860283c72b0cc4343efd8631
Gabriel Ucelli <gabrielucelli@gmail.com>
2017-05-10 20:53:13 -0300

    injecting sessionController

--- src/br/ufes/dwws/vital/control/MenuController.java
+++ src/br/ufes/dwws/vital/control/MenuController.java
@@ -6,30 +6,53 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.enterprise.context.SessionScoped;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
 import javax.enterprise.event.Observes;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import br.ufes.dwws.util.MenuItem;
 import br.ufes.dwws.vital.login.LoginEvent;
+import br.ufes.dwws.vital.login.SessionController;
 
 @Named
-@SessionScoped
+@RequestScoped
 public class MenuController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	private static final Logger logger = Logger.getLogger(MenuController.class.getCanonicalName());
 	private List<MenuItem> menuItems;
+	
+	@Inject
+	SessionController sessionController;
 
 	public MenuController() {
-		
 		logger.log(Level.INFO, "menuController constructor's called");
-		
 		menuItems = new ArrayList<>();
-		menuItems.add(new MenuItem("Item 1", "fa fa-envelope-open", "#"));
-		menuItems.add(new MenuItem("Item N", "fa fa-fire", "#"));
-		menuItems.add(new MenuItem("Item O", "fa fa-heart", "#"));
-		menuItems.add(new MenuItem("Item P", "fa fa-user", "#"));
+	}
+	
+	@PostConstruct
+	private void init()
+	{
+		if(sessionController.hasLoggedUser())
+		{
+			logger.log(Level.INFO, "has logged user");
+			
+			menuItems.add(new MenuItem("Informations", "fa fa-envelope-open", "#"));
+			menuItems.add(new MenuItem("Appointments", "fa fa-heart", "#"));
+			menuItems.add(new MenuItem("Patients", "fa fa-user", "#"));
+			
+		}
+		else
+		{
+			logger.log(Level.INFO, "dont have logged user");
+			
+			menuItems.add(new MenuItem("Home", "fa fa-envelope-open", "#"));
+			menuItems.add(new MenuItem("News", "fa fa-fire", "#"));
+			menuItems.add(new MenuItem("Solutions", "fa fa-heart", "#"));
+			menuItems.add(new MenuItem("Contact", "fa fa-user", "#"));
+		}
 	}
 	
 	public void LoginEventHappens(@Observes LoginEvent event)
